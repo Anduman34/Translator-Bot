d40104022ae6a11d1a546a23dd990827fab64cf4
Andebet <89060174+Anduman34@users.noreply.github.com>
2021-08-17 09:11:25 +0300

    Main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Made with python3
-# (C) @FayasNoushad
+# (C) https://t.me/Be_strong_29
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
-# License -> https://github.com/FayasNoushad/Translator-Bot/blob/main/LICENSE
+# License -> https://github.com/bestrong29/Translator-Bot/blob/main/LICENSE
 
 import os
 from io import BytesIO
@@ -20,20 +20,20 @@ FayasNoushad = Client(
 START_TEXT = """
 Hello {}, I am a google translator telegram bot.
 
-Made by @FayasNoushad
+Made by https://t.me/Be_strong_29
 """
 HELP_TEXT = """
 - Just send a text with language code
 
 example :- `This is a sample text | ml`
 
-Made by @FayasNoushad
+Made by https://t.me/Be_strong_29
 """
 ABOUT_TEXT = """
 - **Bot :** `Translator Bot`
-- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Creator :** [bestrong29](https://telegram.me/bestrong29)
 - **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
-- **Source :** [Click here](https://github.com/FayasNoushad/Translator-Bot)
+- **Source :** [Click here](https://github.com/anduman34/Translator-Bot)
 - **Language :** [Python3](https://python.org)
 - **Library :** [Pyrogram](https://pyrogram.org)
 - **Server :** [Heroku](https://heroku.com)
@@ -54,8 +54,8 @@ HELP_BUTTONS = InlineKeyboardMarkup(
     )
 ABOUT_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        InlineKeyboardButton('Channel', url=' @Andu_info' ),
+        InlineKeyboardButton('Feedback', url=' https://t.me/Be_strong_29' )
         ],[
         InlineKeyboardButton('Home', callback_data='home'),
         InlineKeyboardButton('Help', callback_data='help'),
@@ -69,12 +69,10 @@ CLOSE_BUTTON = InlineKeyboardMarkup(
     )
 TRANSLATE_BUTTON = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
-        ]]
-    )
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='@Andu_info' )
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
 
-@FayasNoushad.on_callback_query()
+@bestrong29on_callback_query()
 async def cb_data(bot, update):
     if update.data == "home":
         await update.message.edit_text(
@@ -98,7 +96,7 @@ async def cb_data(bot, update):
         await update.message.delete()
     
 
-@FayasNoushad.on_message(filters.command(["start"]))
+@be_strong_29.on_message(filters.command(["start"]))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.mention)
     reply_markup = START_BUTTONS
@@ -108,7 +106,7 @@ async def start(bot, update):
         reply_markup=reply_markup
     )
 
-@FayasNoushad.on_message((filters.private | filters.group | ~filters.channel) & filters.text)
+@be_strong_29 .on_message((filters.private | filters.group | ~filters.channel) & filters.text)
 async def translate(bot, update):
     if update.chat.type == "private":
         if " | " in update.text:
@@ -129,7 +127,7 @@ async def translate(bot, update):
         translate = translator.translate(text, dest=language)
         translate_text = f"**Translated to {language}**"
         translate_text += f"\n\n{translate.text}"
-        translate_text += "\n\nMade by @FayasNoushad"
+        translate_text += "\n\nMade by https://t.me/Be_strong_29  "
         if len(translate_text) < 4096:
             await message.edit_text(
                 text=translate_text,
@@ -141,7 +139,7 @@ async def translate(bot, update):
                 translate_file.name = language + ".txt"
                 await update.reply_document(
                     document=translate_file,
-                    caption="Made by @FayasNoushad",
+                    caption="Made by   https://t.me/Be_strong_29",
                     reply_markup=TRANSLATE_BUTTON
                 )
                 await message.delete()
@@ -150,4 +148,4 @@ async def translate(bot, update):
         await message.edit_text("Something wrong. Contact @TheFayas.")
         return
 
-FayasNoushad.run()
+https://t.me/Be_strong_29run()
